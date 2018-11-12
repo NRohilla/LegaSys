c524c59c1f0fd45c8bbbe59378f948398f1dbf1f
vesandeep <sandeepkumar2@virtualemployee.com>
2018-11-12 18:00:24 +0530

    Tab index against view details

---
+++ LegaSys/LegaSys/src/app/layout/resource/resource.model.ts
@@ -0,0 +1,12 @@
+export class resource{
+
+    UserDetailID : number;
+    Firstname : string;
+    Middlename : string;
+    Lastname : string;
+    TotalExp :number;
+    EmailId :string;
+    Remarks : string;
+    Master_Shift_ID :number;
+    LocationAddress :string;
+}
